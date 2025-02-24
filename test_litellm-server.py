17372ce17b82137e287c6972e0c7904acf57d9ca
Jens X Augustsson <jens@augustsson.eu>
2025-02-24 18:24:15 +0100

    Reorg of mode config

--- test_litellm-server.py
+++ test_litellm-server.py
@@ -11,7 +11,7 @@ response = client.chat.completions.create(model="gpt-3.5-turbo-instruct", messag
 print("LiteLLM using OpenAI model:\n", response.choices[0].message.content)
 
 # request to Ollama model
-response = client.chat.completions.create(model="mistral-7b", messages = [
+response = client.chat.completions.create(model="mistral:latest", messages = [
     {
         "role": "user",
         "content": "this is a test request, write a short poem"
